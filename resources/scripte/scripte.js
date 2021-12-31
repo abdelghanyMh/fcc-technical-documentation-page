c25422204399ac5ae56cdd8750da82ca092760d5
abdelghanyMh <wboductqr@relay.firefox.com>
2021-12-31 17:57:47 +0100

    add desktop version

--- resources/scripte/scripte.js
+++ resources/scripte/scripte.js
@@ -1,4 +1,6 @@
+
 // Add the Toggle Functionality with JavaScript
+
 toggleButton = document.getElementById('toggle');
 menu = document.getElementById('navbar');
 /* Toggle mobile menu */
@@ -20,12 +22,13 @@ function toggleMenu() {
     }
 }
 
+
 /*add style to the clicked nav link  */
-// navItems = document.getElementsByClassName('nav-link')
+
 navItems = document.querySelectorAll('.nav-link')
 
 function styleNavItem(event) {
-     console.log(event.currentTarget);
+    console.log(event.currentTarget);
     navItems.forEach(item => {
         if (item.classList.contains('active-nav-item'))
             item.classList.remove('active-nav-item')
@@ -33,12 +36,42 @@ function styleNavItem(event) {
 
     // add style 
     event.currentTarget.classList.add('active-nav-item')
-  
+
 
 }
-/* Event Listeners */
+
+
+/*
+Highlighting current section in navbar
+*/
+const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll("li.nav-link");
+function highlightSection()  {
+    let current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+            // current=section.children[1].getAttribute('href').slice(1)
+        }
+    });
+
+    navItems.forEach((li) => {
+        li.classList.remove("active-nav-item");
+        let id = li.children[0].getAttribute('href').slice(1)
+        if (id==current) {
+            li.classList.add("active-nav-item");
+        }
+    });
+}
+
+
+
+/** Event Listeners */
+
 toggleButton.addEventListener("click", toggleMenu, false);
-// navItems.addEventListener("click", styleNavItem, false);
-navItems.forEach(item => {
-    item.addEventListener('click',styleNavItem,false)
-});
+
+navItems.forEach(item => { item.addEventListener('click', styleNavItem, false) });
+
+window.addEventListener("scroll", highlightSection)
